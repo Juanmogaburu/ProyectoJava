c980b15d08f620e8a6fb311439dbdda132bedbb3
Juanmogaburu <jmogaburu4@gmail.com>
2023-09-04 14:11:29 -0300

    grabo primer version de mi proyecto

---
+++ app.js
@@ -0,0 +1,50 @@
+//declaracion de variables
+///la forma de asignar un nombre usando camelCase
+let nombrePersona; ///declaracion
+let nombre_persona; 
+let nombre1;
+
+///declare 3 variables
+
+///en javascript el ; no es necesario
+// el ; separa una SENTENCIA de codigo de otra
+///el salto de linea separa una SENTENCIA de otra
+
+
+/////TIPOS DE DATOS
+///Number 1   1.5  raiz(2)
+////String "hola" 'hola' `hola`
+
+
+////ASIGNACION
+
+let numerito; ///DECLARACION -> undefined
+
+numerito = 1; // -> number
+numerito = 8; ///ahora numerito almacena solo un 8
+
+let texto; // -> undefined
+
+texto = 'hola como andas'; /// -> string
+texto = "Melina";
+texto = `Pedrito`;
+
+///DECLARACION Y ASIGNACION => INICIALIZACION
+
+let algunNumero = 3;
+
+
+///el no tener nada almacenado javascript lo denomina como un tipo de dato llamado 'undefined'
+
+/// mensaje de dialogo "alert"
+alert ("¡Bienvenido a Appletatch! A continuacion complete el registro.")
+
+//inicio de sesion
+let valor1;
+let valor2;
+let resultado;
+
+valor1 = prompt("Ingrese correo electronico");
+valor2 = prompt("Ingrese una contraseña");
+resultado = parseFloat(valor1) + parseFloat(valor2);
+alert ("Perfecto! Su cuenta ha sido creada")
